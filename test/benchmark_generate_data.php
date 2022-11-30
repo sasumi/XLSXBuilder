a423bd2924805be7760542b0a6a1d54d2717f745
sasumi <cnsasumi@gmail.com>
2022-11-30 18:57:09 +0800

    upd testcase

--- test/benchmark_generate_data.php
+++ test/benchmark_generate_data.php
@@ -1,5 +1,7 @@
 <?php
 
+use function LFPhp\Func\format_size;
+use function LFPhp\Func\pretty_time;
 use function LFPhp\Func\rand_string;
 use function LFPhp\Func\show_progress;
 
@@ -7,13 +9,15 @@ include "../vendor/autoload.php";
 
 $buff = '';
 $tmp = getopt('n:');
-$total = $tmp['n'];
+$total = $tmp['n'] ?: 1000000;
 if(!$total){
 	die('Example: php '.$_SERVER['SCRIPT_NAME'].' -n1000');
 }
+$start_time = time();
+$max_mem = 0;
 $i = $total;
 $sm = ['待激活', '正常', '注销'];
-$data_file = "data_{$total}.csv";
+$data_file = "data_test.csv";
 $fp = fopen($data_file, 'w');
 while($i-- > 0){
 	$buff .= "$i,u_".rand_string(32).",手机尾号".rand(0,$i)."用户,+853-".rand_string(11,'1234567890').",".$sm[array_rand($sm, 1)].",B端导入,\n";
@@ -21,11 +25,13 @@ while($i-- > 0){
 		fwrite($fp, $buff);
 		$buff = '';
 	}
+	$max_mem = max($max_mem, memory_get_usage(true));
 	show_progress($total-$i, $total);
 }
 if($buff){
 	fwrite($fp, $buff);
 }
 fclose($fp);
-echo "DONE";
+echo "DONE, ", date("H:i:s"),PHP_EOL;
+echo "total time:", pretty_time($start_time), "max memeroy size:", format_size($max_mem);
 exit;
--- test/benchmark_pack.php
+++ test/benchmark_pack.php
@@ -8,17 +8,25 @@ use function LFPhp\Func\read_csv_chunk;
 use function LFPhp\Func\show_progress;
 
 include "../vendor/autoload.php";
-dump(format_size(memory_get_usage()));
+
+$csv_file = 'data_test.csv';
+$output = "output_".time().".xlsx";
+$line = file_lines($csv_file);
+
+$start_time = time();
+$mem = memory_get_usage(true);
+echo "Start..., File Lines:$line ",date('H:i:s'),PHP_EOL;
+echo "Process ID:", getmypid(),PHP_EOL;
 
 XLSXBuilder::setTempDir(__DIR__.'/tmp');
 
 $writer = new XLSXBuilder();
 $sheet = $writer->createSheet('Sheet1');
 
-dump(format_size(memory_get_usage()));
-$csv_file = 'data_100.csv';
-$output = "output_".time().".xlsx";
 XLSXBuilder::convertCSV($csv_file, $output);
 
-echo $output,PHP_EOL;
-die('DONE');
+echo "DONE,", date("H:i:s"),PHP_EOL;
+echo "Time Cost:", time()-$start_time, 'sec', PHP_EOL;
+echo "Mem Cost:", format_size(max($mem, memory_get_usage(true))), PHP_EOL;
+echo "Output:", $output,PHP_EOL;
+
